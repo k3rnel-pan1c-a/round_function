a1be3a72e86a4e1a6d1a74e22b906c7e067df428
Anas Badr <anasihabezmc@gmail.com>
2023-02-05 16:18:29 +0300

    No Strings(Final Working Version)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def round_function(num: float, round_to: int = 0) -> float:
 
     num_abs: float = abs(num)
     if round_to < 0:
-        if abs(round_to) > len(str(num_abs).split(".")[0]):
+        if int(num_abs * 10 ** (- round_to + 1)) == 0:
             return 0.0
         new_num = num * 10 ** round_to
         after_decimal_point: float = abs(new_num) - int(abs(new_num))
@@ -24,8 +24,10 @@ def round_function(num: float, round_to: int = 0) -> float:
                 return 0.0
         else:
             new_num: float = num * 10 ** round_to
-            if int(new_num) == 0:
+            if int(abs(new_num)) == 0 and int((abs(new_num) % 1) * 10) == 0 or int(abs(new_num)) == 0 and \
+                    int((abs(new_num) % 1) * 10) < 5:
                 return 0.0
-            return float(format((int(new_num + ((not num > 0) - 0.5)) - (2 * (not num > 0) - 1)) * 10 ** - round_to, "22.15f"))
+            return float(
+                format((int(new_num + ((not num > 0) - 0.5)) - (2 * (not num > 0) - 1)) * 10 ** - round_to, "22.15f"))
 
     return (int((10 ** round_to) * num + ((not num > 0) - 0.5)) - (2 * (not num > 0) - 1)) / 10 ** round_to
